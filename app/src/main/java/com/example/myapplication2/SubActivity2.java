9152265748654e02eadb36f64c38021278a8a2e1
rocidon <43723501+rocidon@users.noreply.github.com>
2022-08-12 00:23:57 +0900

    momtherfucker this program

--- app/src/main/java/com/example/myapplication2/SubActivity2.java
+++ app/src/main/java/com/example/myapplication2/SubActivity2.java
@@ -3,12 +3,25 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubActivity2 extends AppCompatActivity {
+
+    private Button _Map_Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub2);
+
+        _Map_Btn = findViewById(R.id.UnivMapBtn);
+
+        _Map_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
