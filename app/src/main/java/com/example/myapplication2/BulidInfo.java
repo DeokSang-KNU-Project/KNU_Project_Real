96c0189112d4beecc2184d96ebd81d393ddf72bc
rocidon <43723501+rocidon@users.noreply.github.com>
2022-08-27 18:45:21 +0900

    w
    w

---
+++ app/src/main/java/com/example/myapplication2/BulidInfo.java
@@ -0,0 +1,45 @@
+package com.example.myapplication2;
+
+import com.naver.maps.geometry.LatLng;
+
+public class BulidInfo {
+    private String name;
+    private String Info;
+    private String latit;
+    private String longit;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInfo() {
+        return Info;
+    }
+
+    public double getLatit() {
+        double lat = Double.valueOf(latit).doubleValue();
+        return lat;
+    }
+
+    public double getLongit() {
+        double longi = Double.valueOf(longit).doubleValue();
+        return longi;
+    }
+
+    public void setBulid(String name, String Info) {
+        this.name = name;
+        this.Info = Info;
+    }
+
+    public void setLat(String latit, String longit) {
+        this.latit = latit;
+        this.longit = longit;
+    }
+    public LatLng getLat(){
+        LatLng lat = new LatLng(Double.valueOf(latit).doubleValue(), Double.valueOf(longit).doubleValue());
+        return lat;
+    }
+
+}
